968580f448b43f67631c49e38951c5b9b41e0324
114112_Herrado_Tomas <114112@tecnicatura.frc.utn.edu.ar>
2025-11-14 18:35:16 -0300

    commit

--- src/app/services/alquiler.service.ts
+++ src/app/services/alquiler.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Registro } from '../models/alquiler.model';
 import { Persona } from '../models/persona.model';
-import { environment } from '../environments/environment';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
